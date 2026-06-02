ba317eaaeb0422b778e3474ec5edeea757eb0dc1
Marcio Tahyra <marciotahyra@gmail.com>
2026-06-02 17:19:19 -0300

    fix app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def home():
         "produto": "OrbitGuard API",
         "propósito": "Plataforma de monitoramento de telemetria e integridade de microssatélites em órbita baixa (LEO).",
         "ODS_conectado": "ODS 9 - Indústria, Inovação e Infraestrutura",
-        "equipe": "Márcio Hitoshi Tahyra, Aline Fernandes, Camilly Ishida, Julia Galvão, Jéssica Costacurta",
+        "equipe": "Márcio Hitoshi Tahyra, Jéssica Costacurta",
         "status_infraestrutura": "100% Operacional"
     }
 
